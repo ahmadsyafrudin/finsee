25c6109ec9d06cf607cefb4277e595e73364b317
Ahmad Syafrudin <sfrmd4@gmail.com>
2022-10-19 02:22:11 +0700

    #9 update all package in requirements.txt

--- src/finsee/utils/models.py
+++ src/finsee/utils/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class CreatedModified(models.Model):
-
+    id = models.AutoField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
